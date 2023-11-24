ee824055c472667ba608f17080ce1f97d31ef524
ActualM <92734448+ActualM@users.noreply.github.com>
2023-11-24 21:50:17 +0300

    Add files via upload

---
+++ pizza/assets/js/index.js
@@ -0,0 +1,22 @@
+document.getElementById("modal_popup").addEventListener("click", function(){ 
+    document.getElementById("my_modal").classList.add("open")
+})
+document.getElementById("modal_popup_sec").addEventListener("click", function(){ 
+    document.getElementById("my_modal").classList.add("open")
+})
+document.getElementById("modal_popup_third").addEventListener("click", function(){ 
+    document.getElementById("my_modal").classList.add("open")
+})
+document.getElementById("modal_popup_fourth").addEventListener("click", function(){ 
+    document.getElementById("my_modal").classList.add("open")
+})
+document.getElementById("modal_popup_fifth").addEventListener("click", function(){ 
+    document.getElementById("my_modal").classList.add("open")
+})
+document.getElementById("modal_popup_sixth").addEventListener("click", function(){ 
+    document.getElementById("my_modal").classList.add("open")
+})
+
+document.getElementById("closeModal_btn").addEventListener("click", function(){ 
+    document.getElementById("my_modal").classList.remove("open")
+})
